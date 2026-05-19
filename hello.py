2935640cb38c053e8340742be4d87c7ffd6dff98
Khadija_dev <elfadilkhadija072@gmail.com>
2026-05-19 11:23:53 +0100

    Ajout d'un échec volontaire pour le test

--- hello.py
+++ hello.py
@@ -3,11 +3,12 @@ print("=" * 40)
 print("Bienvenue dans mon premier job Jenkins !")
 print("=" * 40)
 
-nom = input("Quel est ton nom ? ")
-print(f"Bonjour {nom}, ton job Jenkins a réussi !")
+nom = "Khadija"  # On définit directement le nom ici
+print(f"Bonjour {nom}")
 
 # Petit calcul
 a = 10
 b = 5
 print(f"{a} + {b} = {a + b}")
 print(f"{a} - {b} = {a - b}")
+assert 1 + 1 == 100, "❌ Le test a échoué intentionnellement"
